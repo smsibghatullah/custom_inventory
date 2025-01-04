efe3490065718fa0c276c27fa8a18027bf21b7d7
Sibghatullah <smsighatullah@gmail.com>
2025-01-04 14:59:45 +0500

    fixes

--- custom_inventory/models/sale_order.py
+++ custom_inventory/models/sale_order.py
@@ -302,5 +302,89 @@ class SaleOrderLine(models.Model):
                 sku_ids = self.env['sku.type.master'].search([]) 
                 line.sku_ids = sku_ids
             line.product_template_id = line.product_id.product_tmpl_id
-              
-    
+
+class AccountTax(models.Model):
+    _inherit = 'account.tax'
+
+    @api.model
+    def _compute_taxes_for_single_line(self, base_line, handle_price_include=True, include_caba_tags=False,
+                                       early_pay_discount_computation=None, early_pay_discount_percentage=None):
+        print("saaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa45454")
+        print(self)
+        print("assss")
+        print(base_line)
+        print("aaa")
+        if not base_line['record'].order_id.brand_id.is_tax_show:
+            base_line['taxes'] = self.env['account.tax'].search([('id','=',0)])
+        print(base_line['record'].order_id.brand_id.is_tax_show)
+        print("assssssa22aa")
+        print(base_line)
+        orig_price_unit_after_discount = base_line['price_unit'] * (1 - (base_line['discount'] / 100.0))
+        price_unit_after_discount = orig_price_unit_after_discount
+        taxes = base_line['taxes']._origin
+        currency = base_line['currency'] or self.env.company.currency_id
+        rate = base_line['rate']
+
+        if early_pay_discount_computation in ('included', 'excluded'):
+            remaining_part_to_consider = (100 - early_pay_discount_percentage) / 100.0
+            price_unit_after_discount = remaining_part_to_consider * price_unit_after_discount
+
+        if taxes:
+            taxes_res = taxes.with_context(**base_line['extra_context']).compute_all(
+                price_unit_after_discount,
+                currency=currency,
+                quantity=base_line['quantity'],
+                product=base_line['product'],
+                partner=base_line['partner'],
+                is_refund=base_line['is_refund'],
+                handle_price_include=base_line['handle_price_include'],
+                include_caba_tags=include_caba_tags,
+            )
+
+            to_update_vals = {
+                'tax_tag_ids': [Command.set(taxes_res['base_tags'])],
+                'price_subtotal': taxes_res['total_excluded'],
+                'price_total': taxes_res['total_included'],
+            }
+
+            if early_pay_discount_computation == 'excluded':
+                new_taxes_res = taxes.with_context(**base_line['extra_context']).compute_all(
+                    orig_price_unit_after_discount,
+                    currency=currency,
+                    quantity=base_line['quantity'],
+                    product=base_line['product'],
+                    partner=base_line['partner'],
+                    is_refund=base_line['is_refund'],
+                    handle_price_include=base_line['handle_price_include'],
+                    include_caba_tags=include_caba_tags,
+                )
+                for tax_res, new_taxes_res in zip(taxes_res['taxes'], new_taxes_res['taxes']):
+                    delta_tax = new_taxes_res['amount'] - tax_res['amount']
+                    tax_res['amount'] += delta_tax
+                    to_update_vals['price_total'] += delta_tax
+
+            tax_values_list = []
+            for tax_res in taxes_res['taxes']:
+                tax_amount = tax_res['amount'] / rate
+                if self.company_id.tax_calculation_rounding_method == 'round_per_line':
+                    tax_amount = currency.round(tax_amount)
+                tax_rep = self.env['account.tax.repartition.line'].browse(tax_res['tax_repartition_line_id'])
+                tax_values_list.append({
+                    **tax_res,
+                    'tax_repartition_line': tax_rep,
+                    'base_amount_currency': tax_res['base'],
+                    'base_amount': currency.round(tax_res['base'] / rate),
+                    'tax_amount_currency': tax_res['amount'],
+                    'tax_amount': tax_amount,
+                })
+
+        else:
+            price_subtotal = currency.round(price_unit_after_discount * base_line['quantity'])
+            to_update_vals = {
+                'tax_tag_ids': [Command.clear()],
+                'price_subtotal': price_subtotal,
+                'price_total': price_subtotal,
+            }
+            tax_values_list = []
+
+        return to_update_vals, tax_values_list
