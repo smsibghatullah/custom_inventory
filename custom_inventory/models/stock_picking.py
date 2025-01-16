34f1d71d8518ebca5e9530a83734ead36b893c1a
mubeen1020 <m.mubeen1020@gmail.com>
2025-01-16 16:43:41 +0500

    working on delivery

--- custom_inventory/models/stock_picking.py
+++ custom_inventory/models/stock_picking.py
@@ -1,32 +1,48 @@
-from odoo import models, fields,api
+from odoo import models, fields, api
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('waiting', 'Waiting Another Operation'),
-        ('confirmed', 'Waiting'),
-        ('assigned', 'Ready'),
-        ('picking', 'Picking'),
-        ('picked_up', 'Picked Up'),
-        ('done', 'Done'),
+        ('pick_pack', 'Pick & Pack'),
+        ('ready_for_pickup', 'Ready for Pickup'),
+        ('picked_up_by_logistic', 'Picked Up by Logistic Car'),
+        ('in_transit', 'In Transit'),
+        ('delivered', 'Delivered'),
+        ('pickup_by_buyer', 'Pickup by Buyer'),
         ('cancel', 'Cancelled'),
-    
     ], string='Status', compute='_compute_state',
         copy=False, index=True, readonly=True, store=True, tracking=True,
-        help=" * Draft: The transfer is not confirmed yet. Reservation doesn't apply.\n"
-             " * Waiting another operation: This transfer is waiting for another operation before being ready.\n"
-             " * Waiting: The transfer is waiting for the availability of some products.\n(a) The shipping policy is \"As soon as possible\": no product could be reserved.\n(b) The shipping policy is \"When all products are ready\": not all the products could be reserved.\n"
-             " * Ready: The transfer is ready to be processed.\n(a) The shipping policy is \"As soon as possible\": at least one product has been reserved.\n(b) The shipping policy is \"When all products are ready\": all product have been reserved.\n"
-             " * Done: The transfer has been processed.\n"
+        help=" * Draft: The transfer is not confirmed yet.\n"
+             " * Pick & Pack: The transfer is in the picking process.\n"
+             " * Ready for Pickup: The transfer is ready for pickup.\n"
+             " * Picked Up by Logistic Car: The transfer has been picked up by the logistics car.\n"
+             " * In Transit: The transfer is on the way to the delivery location.\n"
+             " * Delivered: The transfer has been delivered.\n"
+             " * Pickup by Buyer: The buyer picked up the transfer.\n"
              " * Cancelled: The transfer has been cancelled.")
 
-    def action_picking(self):
+    def action_pick_pack(self):
         for record in self:
-            record.state = 'picking'
+            record.state = 'pick_pack'
 
-    def action_picked_up(self):
+    def action_ready_for_pickup(self):
         for record in self:
-            record.state = 'picked_up'
+            record.state = 'ready_for_pickup'
+
+    def action_picked_up_by_logistic(self):
+        for record in self:
+            record.state = 'picked_up_by_logistic'
+
+    def action_in_transit(self):
+        for record in self:
+            record.state = 'in_transit'
+
+    def action_delivered(self):
+        for record in self:
+            record.state = 'delivered'
+
+    def action_pickup_by_buyer(self):
+        for record in self:
+            record.state = 'pickup_by_buyer'
