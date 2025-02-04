21ebe2bc882630f671a695450232fe9e11092a00
mubeen1020 <m.mubeen1020@gmail.com>
2025-02-04 16:49:05 +0500

    working

--- custom_inventory/__manifest__.py
+++ custom_inventory/__manifest__.py
@@ -26,7 +26,7 @@ Developed by DSM (Dynamic Solution Maker). For more details, visit:
     'category': 'Inventory',
     'version': '1.0',
 
-    'depends': ['base','stock','sale','purchase','account','purchase_stock','crm', 'sale_crm','delivery','l10n_au','l10n_nz'],
+    'depends': ['base','stock','sale','purchase','account','purchase_stock','crm', 'sale_crm','delivery'],
 
     'data': [
          'data/data.xml',
