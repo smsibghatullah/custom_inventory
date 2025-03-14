0749d70e4f051c4872aa6ae1a457201ae0178d44
mubeen1020 <m.mubeen1020@gmail.com>
2025-03-14 15:57:14 +0500

    working

--- custom_inventory/models/res_company.py
+++ custom_inventory/models/res_company.py
@@ -9,3 +9,16 @@ class ResCompany(models.Model):
         string='Brands',
         help='Select the brands associated with this company'
     )
+
+    tag_ids = fields.One2many(
+        'crm.tag',  
+        'company_id', 
+        string='Tags',
+        help='Select the brands associated with this company'
+    )
+
+class Tag(models.Model):
+    _inherit = "crm.tag"
+
+    company_id = fields.Many2one('res.company', string="Company")
+
