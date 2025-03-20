fb83a217b4379203c694b04c40974ea103ea60bd
mubeen1020 <m.mubeen1020@gmail.com>
2025-03-20 15:27:24 +0500

    working

--- custom_inventory/models/res_users.py
+++ custom_inventory/models/res_users.py
@@ -22,3 +22,17 @@ class ResUsers(models.Model):
         help='Select the Categories associated with this company',
         domain="[('company_id', 'in', company_ids)]",
     )
+
+    @api.onchange('company_ids')
+    def _onchange_company_ids(self):
+        """Remove only the tags and categories that do not match the selected companies."""
+        if self.company_ids:
+            company_ids = self.company_ids.ids
+
+            valid_tags = self.env['crm.tag'].search([('company_id', 'in', company_ids)]).ids
+            valid_categories = self.env['sku.type.master'].search([('company_id', 'in', company_ids)]).ids
+
+            self.tag_ids = [(6, 0, list(set(self.tag_ids.ids) & set(valid_tags)))]
+
+            self.category_ids = [(6, 0, list(set(self.category_ids.ids) & set(valid_categories)))]
+               
