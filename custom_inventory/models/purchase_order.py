ca0ca397749b2748c6bd0684d5bc43c524dd4e93
mubeen1020 <m.mubeen1020@gmail.com>
2025-03-27 12:23:10 +0500

    working

--- custom_inventory/models/purchase_order.py
+++ custom_inventory/models/purchase_order.py
@@ -218,30 +218,40 @@ class PurchaseOrder(models.Model):
                 # line.sku_ids  = [(6, 0, [])]
 
 
+
     @api.depends('brand_id')
     def _compute_selection_field(self):
+        selection_fields = []
+        random_number = random.randint(100000, 999999)
         for line in self:
-            purchase_selection_fields = []
-            random_number = random.randint(100000, 999999)
             if line.brand_id:
                 for item in line.brand_id.purchase_selection_fields:
-                    selection_id = self.env['dynamic.purchaseorder.selection.key'].create({
-                        'selection_field': item.selection_field,
-                        'sale_order_id': line.id,
-                        'sale_random_key': random_number
-                    })
-                    
+                    options = {}
+                    if line.id:
+                        selection_id = self.env['purchase.dynamic.purchaseorder.selection.key'].search([('purchase_order_id','=',line.id)])
+                        if not selection_id:
+                            item_sale_item_selection = {'selection_field': item.selection_field,
+                                                        'purchase_order_id': line.id,'sale_random_key': random_number}
+                            selection_id = self.env['purchase.dynamic.purchaseorder.selection.key'].create(item_sale_item_selection)
+                    else:
+                        item_sale_item_selection = {'selection_field': item.selection_field,
+                        'purchase_order_id':line.id}
+                        selection_id = self.env['purchase.dynamic.purchaseorder.selection.key'].create(item_sale_item_selection)
+                    # selection_value = self.env['dynamic.field.selection.values.sale'].search([('sale_random_key','=',selection_id.random_number)])
+                    # if selection_value:
                     for value in item.selection_value:
-                        new_option = self.env['dynamic.field.selection.values.purchase'].create({
-                            'value_field': value.value_field,
-                            'key_field': selection_id.id,
-                            'key_field_parent': item.selection_field
-                        })
-                        
+                       options = {
+                        'value_field': value.value_field,
+                        'key_field': selection_id.id,
+                        'key_field_parent': item.selection_field  
+                        }
+                      
+                       new_option = self.env['purchase.dynamic.field.selection.values.purchase'].create(options)
+                    
                     if not selection_id.selected_value:
                         selection_id.selected_value = new_option
-                    purchase_selection_fields.append(selection_id.id)
-            line.selection_fields = [(6, 0, purchase_selection_fields)]
+                    selection_fields.append(selection_id.id)
+            line.selection_fields = [(6, 0, selection_fields)]
 
     def create_journal_entry(self):
         for order in self:
@@ -408,7 +418,6 @@ class PurchaseOrderEmailWizard(models.TransientModel):
                 'attachment_ids': [(6, 0, [attachment.id])],
             }
             mail = self.env['mail.mail'].create(mail_values)
-
             mail.send()
 
         except Exception as e:
