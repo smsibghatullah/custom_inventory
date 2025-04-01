70bd333d5d616a0d83cf3b9e02963325f7830332
mubeen1020 <m.mubeen1020@gmail.com>
2025-04-01 20:32:11 +0500

    working

--- custom_inventory/models/product_category.py
+++ custom_inventory/models/product_category.py
@@ -4,20 +4,20 @@ class ProductCategory(models.Model):
     _inherit = "product.category"
 
     sale_debit_account_id = fields.Many2one(
-        'account.account', string="Sale Debit Account", domain=[('deprecated', '=', False)]
+        'account.account', string="Sale Debit Account", domain=[('deprecated', '=', False)],company_dependent=True
     )
     sale_credit_account_id = fields.Many2one(
-        'account.account', string="Sale Credit Account", domain=[('deprecated', '=', False)]
+        'account.account', string="Sale Credit Account", domain=[('deprecated', '=', False)],company_dependent=True
     )
     sale_journal_id = fields.Many2one(
-        'account.journal', string="Sale Journal", domain=[('type', '=', 'sale')]
+        'account.journal', string="Sale Journal", domain=[('type', '=', 'sale')],company_dependent=True
     )
     purchase_debit_account_id = fields.Many2one(
-        'account.account', string="Purchase Debit Account", domain=[('deprecated', '=', False)]
+        'account.account', string="Purchase Debit Account", domain=[('deprecated', '=', False)],company_dependent=True
     )
     purchase_credit_account_id = fields.Many2one(
-        'account.account', string="Purchase Credit Account", domain=[('deprecated', '=', False)]
+        'account.account', string="Purchase Credit Account", domain=[('deprecated', '=', False)],company_dependent=True
     )
     purchase_journal_id = fields.Many2one(
-        'account.journal', string="Purchase Journal", domain=[('type', '=', 'purchase')]
+        'account.journal', string="Purchase Journal", domain=[('type', '=', 'purchase')],company_dependent=True
     )
--- custom_inventory/models/res_users.py
+++ custom_inventory/models/res_users.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
-
     
 
     tag_ids = fields.Many2many(
@@ -47,9 +46,10 @@ class ResPartner(models.Model):
     current_company_id = fields.Many2one(
         'res.company',
         compute='_compute_current_company',
-        store=True
+        store=False
     )
 
+    @api.depends('company_id')
     def _compute_current_company(self):
         for record in self:
             record.current_company_id = self.env.company
