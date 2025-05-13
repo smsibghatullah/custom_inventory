b20ffa4b8473185fb87d0614ff593904c074aa02
mubeen1020 <m.mubeen1020@gmail.com>
2025-05-13 17:06:26 +0500

    working

--- akahu_bank_integration/models/account_move.py
+++ akahu_bank_integration/models/account_move.py
@@ -27,7 +27,7 @@ class AccountMove(models.Model):
             'target': 'new',
             'context': {
                 'default_invoice_id': self.id,
-                'default_transaction_amount': match.amount_due if match.match_status == 'partial' else match.amount,
+                'default_transaction_amount': match.amount_due,
             }
         }
 
