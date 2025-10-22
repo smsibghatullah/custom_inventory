e64053f288a9e716c7964b1faf2c5c8960feb4c1
mubeen1020 <m.mubeen1020@gmail.com>
2025-10-22 08:54:56 +0500

    working

--- custom_inventory/models/report.py
+++ custom_inventory/models/report.py
@@ -1,36 +1,36 @@
-from odoo import http
-from odoo.http import request
-from odoo.addons.web.controllers.report import ReportController  # ✅ Correct import
+# from odoo import http
+# from odoo.http import request
+# from odoo.addons.web.controllers.report import ReportController  # ✅ Correct import
 
-class CustomReportController(ReportController):
+# class CustomReportController(ReportController):
 
-    @http.route(['/report/download'], type='http', auth="user")
-    def report_download(self, data, token):
-        import json
+#     @http.route(['/report/download'], type='http', auth="user")
+#     def report_download(self, data, token):
+#         import json
 
-        request_content = json.loads(data)
-        url, report_type = request_content[0], request_content[1]
+#         request_content = json.loads(data)
+#         url, report_type = request_content[0], request_content[1]
 
-        if report_type in ['qweb-pdf', 'qweb-text']:
-            reportname = url.split('/report/pdf/')[1].split('?')[0]
-            docids = reportname.split('/')[-1]
-            model = reportname.split('/')[0]
+#         if report_type in ['qweb-pdf', 'qweb-text']:
+#             reportname = url.split('/report/pdf/')[1].split('?')[0]
+#             docids = reportname.split('/')[-1]
+#             model = reportname.split('/')[0]
 
-            record = request.env[model].browse(int(docids))
-            filename = ''
+#             record = request.env[model].browse(int(docids))
+#             filename = ''
 
-            # Customize file name based on model
-            if model == 'sale.order':
-                filename = record.name
-            elif model in ['account.move', 'account.invoice']:  # account.invoice old hai, 17 me account.move hai
-                filename = record.name or 'invoice'
-            elif model == 'purchase.order':
-                filename = record.name
-            else:
-                filename = model.replace('.', '_') + '_' + str(record.id)
+#             # Customize file name based on model
+#             if model == 'sale.order':
+#                 filename = record.name
+#             elif model in ['account.move', 'account.invoice']:  # account.invoice old hai, 17 me account.move hai
+#                 filename = record.name or 'invoice'
+#             elif model == 'purchase.order':
+#                 filename = record.name
+#             else:
+#                 filename = model.replace('.', '_') + '_' + str(record.id)
 
-            filename = filename.replace('/', '_') + '.pdf'
+#             filename = filename.replace('/', '_') + '.pdf'
 
-            response = super().report_download(data, token)
-            response.headers['Content-Disposition'] = 'attachment; filename=%s' % filename
-            return response
+#             response = super().report_download(data, token)
+#             response.headers['Content-Disposition'] = 'attachment; filename=%s' % filename
+#             return response
