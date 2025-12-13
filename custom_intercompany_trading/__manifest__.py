6c6078aaae075ecd1ea0ff4e64e2f7536b7072ed
smsibghatullah <>
2025-12-13 19:00:53 +0500

    chore: Filter PO as per selected fields

--- custom_intercompany_trading/__manifest__.py
+++ custom_intercompany_trading/__manifest__.py
@@ -18,7 +18,7 @@ Long description of module's purpose
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale', 'base_accounting_kit', 'custom_inventory'],
+    'depends': ['sale', 'base_accounting_kit', 'custom_inventory', 'purchase'],
 
     # always loaded
     'data': [
